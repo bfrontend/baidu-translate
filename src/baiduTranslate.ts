38e8c1caceae7563b6b2d50537bbd46c9915ad33
webxmsj <webxmsj@163.com>
2023-11-22 18:04:41 +0800

    fix: 修复语言参数错误的问题

--- src/baiduTranslate.ts
+++ src/baiduTranslate.ts
@@ -7,99 +7,128 @@ import { ITranslate, ITranslateOptions } from 'comment-translate-manager';
 
 const PREFIXCONFIG = 'baiduTranslate';
 
+const langMaps: Map<string, string> = new Map([
+  ["zh-CN", "zh"],
+  ["zh-TW", "cht"],
+  ["ko", "kor"],
+  ["ja", "jp"],
+  ["fr", "fra"],
+  ["fr", "fra"],
+  ["es", "spa"],
+  ["es", "ara"],
+  ["bg", "bul"],
+  ["et", "est"],
+  ["da", "dan"],
+  ["fi", "fin"],
+  ["ro", "rom"],
+  ["sl", "slo"],
+  ["sv", "swe"],
+  ["vi", "vie"],
+]);
+
 function md5Str(text: string) {
-    return crypto.createHash('md5')
-    .update(text)
-    .digest('hex');
+  return crypto.createHash("md5").update(text).digest("hex");
+}
+
+function convertLang(src: string) {
+  if (langMaps.has(src)) {
+    return langMaps.get(src);
+  }
+  return src;
 }
 
 function sendRequest<T>(url: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-      https.get(url, res => {
-        if (res.statusCode === 200) {
-          let list = [];
-          res.on('data', chunk => {
-            list.push(chunk);
-          });
-          res.on('end', () => {
-            resolve(JSON.parse(Buffer.concat(list).toString()));
-          });
-        }
-      });
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 200) {
+        let list = [];
+        res.on("data", (chunk) => {
+          list.push(chunk);
+        });
+        res.on("end", () => {
+          resolve(JSON.parse(Buffer.concat(list).toString()));
+        });
+      }
     });
-  }
+  });
+}
 
 export function getConfig<T>(key: string): T | undefined {
-    let configuration = workspace.getConfiguration(PREFIXCONFIG);
-    return configuration.get<T>(key);
+  let configuration = workspace.getConfiguration(PREFIXCONFIG);
+  return configuration.get<T>(key);
 }
 
 interface BaiduTranslateOption {
-    appId?: string;
-    appKey?: string;
+  appId?: string;
+  appKey?: string;
 }
 interface Response {
-    form: string;
-    to: string;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    trans_result: {
-        src: string;
-        dst: string
-    }[]
+  form: string;
+  to: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  trans_result: {
+    src: string;
+    dst: string;
+  }[];
 }
 
 export class BaiduTranslate implements ITranslate {
-    get maxLen(): number {
-        return 2000;
-    }
+  get maxLen(): number {
+    return 2000;
+  }
 
-    private _defaultOption: BaiduTranslateOption;
-    constructor() {
+  private _defaultOption: BaiduTranslateOption;
+  constructor() {
+    this._defaultOption = this.createOption();
+    workspace.onDidChangeConfiguration(async (eventNames) => {
+      if (eventNames.affectsConfiguration(PREFIXCONFIG)) {
         this._defaultOption = this.createOption();
-        workspace.onDidChangeConfiguration(async eventNames => {
-            if (eventNames.affectsConfiguration(PREFIXCONFIG)) {
-                this._defaultOption = this.createOption();
-            }
-        });
+      }
+    });
+  }
+
+  createOption() {
+    const defaultOption: BaiduTranslateOption = {
+      appId: getConfig<string>("appId"),
+      appKey: getConfig<string>("appKey"),
+    };
+    return defaultOption;
+  }
+
+  async translate(
+    content: string,
+    { to = "auto", from = "auto" }: ITranslateOptions
+  ) {
+    const { appId, appKey } = this._defaultOption;
+    if (!appId || !appKey) {
+      throw new Error("Please check the configuration of appId and appKey!");
     }
+    const data = {
+      q: content,
+      from: convertLang(from),
+      to: convertLang(to),
+      appid: appId,
+      salt: Date.now() + "",
+      sign: "",
+    };
+    data.sign = md5Str(`${appId}${content}${data.salt}${appKey}`);
+    const url = "https://api.fanyi.baidu.com/api/trans/vip/translate";
+    let res = await sendRequest<Response>(
+      `${url}?${querystring.stringify(data)}`
+    );
+    return res.trans_result[0].dst;
+  }
 
-    createOption() {
-        const defaultOption:BaiduTranslateOption = {
-            appId: getConfig<string>('appId'),
-            appKey: getConfig<string>('appKey')
-        };
-        return defaultOption;
-    }
+  link(content: string, { to = "auto", from = "auto" }: ITranslateOptions) {
+    let str = `https://fanyi.baidu.com/#${from}/${to}/${encodeURIComponent(
+      content
+    )}`;
+    return `[baidu](${str})`;
+  }
 
-    async translate(content: string, { to = 'auto', from = 'auto' }: ITranslateOptions) {
-
-        const { appId, appKey } = this._defaultOption;
-        if(!appId || !appKey) {
-            throw new Error('Please check the configuration of appId and appKey!');
-        }
-        const data = {
-            q: content,
-            from,
-            to: 'zh',
-            appid: appId,
-            salt: Date.now() + '',
-            sign: ''
-        };
-        data.sign = md5Str(`${appId}${content}${data.salt}${appKey}`);
-        const url = 'https://api.fanyi.baidu.com/api/trans/vip/translate';
-        let res = await sendRequest<Response>(`${url}?${querystring.stringify(data)}`);
-        return res.trans_result[0].dst;
-    }
-
-
-    link(content: string, { to = 'auto', from = 'auto' }: ITranslateOptions) {
-        let str = `https://fanyi.baidu.com/#${from}/${to}/${encodeURIComponent(content)}`;
-        return `[baidu](${str})`;
-    }
-
-    isSupported(src: string) {
-        return true;
-    }
+  isSupported(src: string) {
+    return true;
+  }
 }
 
 
